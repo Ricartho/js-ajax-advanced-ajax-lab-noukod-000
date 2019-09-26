89aab9a26a55990be226ff95a44ae53d33afedd1
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-26 22:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(event){
-  Handlebars.RegisterPartial('author-partial-template',document.getElementById('author-partial-template'));  
+  Handlebars.RegisterPartial('author-partial-template',document.getElementById('author-partial-template').innerHTML);  
 });
 
 function showRepositories(event, data) {
